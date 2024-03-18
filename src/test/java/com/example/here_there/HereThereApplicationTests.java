75d009aa6458d087b916fee4d1722fbc78307354
csh0927 <sung060927@gmail.com>
2024-03-18 09:56:04 +0900

    프로젝트 세팅

---
+++ src/test/java/com/example/here_there/HereThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.here_there;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereThereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
